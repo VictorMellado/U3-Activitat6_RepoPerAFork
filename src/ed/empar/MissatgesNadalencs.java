3ec81b12e4d15cef43d28b1b9bcf9778b855cd94
Victor Mellado <victormm122003@gmail.com>
2021-12-19 14:13:04 +0100

    [MOD] Metode de Víctor Mellado

--- src/ed/empar/MissatgesNadalencs.java
+++ src/ed/empar/MissatgesNadalencs.java
@@ -157,7 +157,25 @@ public class MissatgesNadalencs {
 	}
 
 	private static void AlumneFila3PC2() {
-
+		System.out.println("Hola, sóc Víctor Mellado");
+		System.out.println("BON NADAL!");
+		System.out.println(" FELIÇ ANY NOU!");
+		int i,j,k,n;
+		n=5;
+		for(i=1; i<n+(n/2); i++){
+			for(j=n+(n/2); j>i; j--){
+				System.out.print(" ");}
+			for(k=1; k<=2*i-1; k++){
+				System.out.print("*");}
+			System.out.println("");
+		}
+		for(i=1; i<n-(n/2); i++){
+			for(j=n+(n/2); j>1; j--){
+				System.out.print(" ");}
+			for(k=n/2; k<=(n/2)+1; k++){
+				System.out.print("*");}
+			System.out.println("");
+		}
 	}
 
 	private static void AlumneFila3PC3() {
